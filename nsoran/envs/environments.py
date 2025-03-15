8933d0628bfb3950b3549818f8f1a3e470236636
Bolun Zhang <bolun_zhangzbl@outlook.com>
2025-03-15 19:10:45 +0800

    15/03/2025; main; gitignore environements

--- nsoran/envs/environments.py
+++ nsoran/envs/environments.py
@@ -16,10 +16,11 @@ from nsoran.utils import *
 class ORANSimEnv(gym.Env):
 
     def __init__(self, args):
-
+        self.args = args
         self.num_enbs = args.num_enbs
         self.current_step = 0
         self.time = 0
+        self.latest_time = None
         self.done = False
 
         # scenario callbacks
@@ -34,7 +35,7 @@ class ORANSimEnv(gym.Env):
         self.data_parser = DataParser(args)
 
         # reward
-        self.reward_weights = [0.4, 0.4, 0.1, 0.1]
+        self.reward_weights = [0.4, 0.4, 0.1, -0.1]
         self.reward_threshold = int(1e6)
 
         # link to fifo
@@ -48,7 +49,7 @@ class ORANSimEnv(gym.Env):
         assert len(action) == self.num_enbs
 
         next_state, lastest_time = self._get_obs()
-        reward, _ = self._get_reward()
+        reward, _ = self._get_reward(next_state)
         self.done = self._get_done(reward)
 
         self._send_action(action, lastest_time)
@@ -60,37 +61,48 @@ class ORANSimEnv(gym.Env):
         self.done = False
 
     def _get_obs(self):
-        df_state, latest_time = self.data_parser.read_kpms('tp')
+        df_state = self.data_parser.aggregate_kpms()
+        self.latest_time = self.data_parser.last_read_time
 
         # Add Tx power from ORAN scenario
-        max_bytes = self.args.num_enbs * 4
-        try:
-            data_tx_power = os.read(self.fifo1, max_bytes).decode("utf-8", errors="ignore").strip()
-            data_tx_power = list(map(int, data_tx_power.split(",")))
-        except (OSError, UnicodeDecodeError, ValueError):
-            print("Warning: Error reading Tx power, using default values.")
-            data_tx_power = [self.args.active_power] * self.num_enbs  # Default fallback
-
-        df_state['tx_power'] = data_tx_power
+        data_tx_power = self._read_fifo(self.fifo1_path, default_value=self.args.active_power)
+        df_state['tx_power'] = data_tx_power[:len(df_state)]
 
         data_state = df_state.to_numpy()
 
-        return data_state, latest_time
+        return data_state, self.latest_time
 
-    def _get_reward(self):
-        df_reward, latest_time = self.data_parser.read_kpms('tp')
+    def _get_reward(self, df_state):
 
-        if df_reward.empty:
-            return 0, latest_time
-
-        data_reward = df_reward.to_numpy()
+        data_reward = df_state.to_numpy()
         reward = np.dot(data_reward, self.reward_weights).sum()
 
-        return reward, latest_time
+        return reward
 
     def _get_done(self, reward):
 
         return True if reward >= self.reward_threshold else False
+
+    def _read_fifo(self, fifo_path, default_value):
+        """Read from FIFO safely, returning default values on error."""
+        try:
+            if not os.path.exists(fifo_path):
+                raise FileNotFoundError(f"FIFO {fifo_path} does not exist.")
+
+            with open(fifo_path, "r") as fifo:
+                data = fifo.read().strip()
+                return list(map(int, data.split(",")))
+        except (OSError, ValueError, FileNotFoundError):
+            print(f"Warning: Error reading {fifo_path}, using default values.")
+            return [default_value] * self.num_enbs
+
+    def _write_fifo(self, fifo_path, message):
+        """Write data to FIFO safely."""
+        try:
+            with open(fifo_path, "w") as fifo:
+                fifo.write(message + "\n")
+        except OSError as e:
+            print(f"Error writing to FIFO {fifo_path}: {e}")
 
     def _send_action(self, action, timestamp):
         enbs_active_status = np.array(action)
