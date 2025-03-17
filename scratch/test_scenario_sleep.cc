16aa83aca7d530a449c737cbbca3051d92dd6627
Bolun Zhang <bolun_zhangzbl@outlook.com>
2025-03-17 14:56:38 +0800

    17/03/2025; semaphore; test_scenario_sleep debug 7th

--- scratch/test_scenario_sleep.cc
+++ scratch/test_scenario_sleep.cc
@@ -453,6 +453,53 @@ void NetworkScenario::create_ue_applications()
     }
 }
 
+ void NetworkScenario::printStats(FlowMonitorHelper &flowmon_helper, bool perFlowInfo)
+{
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon_helper.GetClassifier());
+    std::string proto;
+    Ptr<FlowMonitor> monitor = flowmon_helper.GetMonitor ();
+    std::map < FlowId, FlowMonitor::FlowStats > stats = monitor->GetFlowStats();
+    double totalTimeReceiving;
+    uint64_t totalPacketsReceived, totalPacketsDropped, totalBytesReceived;
+
+    totalBytesReceived = 0, totalPacketsDropped = 0, totalPacketsReceived = 0, totalTimeReceiving = 0;
+    for (std::map< FlowId, FlowMonitor::FlowStats>::iterator flow = stats.begin(); flow != stats.end(); flow++)
+    {
+      Ipv4FlowClassifier::FiveTuple  t = classifier->FindFlow(flow->first);
+      switch(t.protocol)
+       {
+       case(6):
+           proto = "TCP";
+           break;
+       case(17):
+           proto = "UDP";
+           break;
+       default:
+           exit(1);
+       }
+       totalBytesReceived += (double) flow->second.rxBytes * 8;
+       totalTimeReceiving += flow->second.timeLastRxPacket.GetSeconds ();
+       totalPacketsReceived += flow->second.rxPackets;
+       totalPacketsDropped += flow->second.txPackets - flow->second.rxPackets;
+       if (perFlowInfo) {
+         std::cout << "FlowID: " << flow->first << " (" << proto << " "
+                   << t.sourceAddress << " / " << t.sourcePort << " --> "
+                   << t.destinationAddress << " / " << t.destinationPort << ")" << std::endl;
+         std::cout << "  Tx Bytes: " << flow->second.txBytes << std::endl;
+         std::cout << "  Rx Bytes: " << flow->second.rxBytes << std::endl;
+         std::cout << "  Tx Packets: " << flow->second.txPackets << std::endl;
+         std::cout << "  Rx Packets: " << flow->second.rxPackets << std::endl;
+         std::cout << "  Time LastRxPacket: " << flow->second.timeLastRxPacket.GetSeconds () << "s" << std::endl;
+         std::cout << "  Lost Packets: " << flow->second.lostPackets << std::endl;
+         std::cout << "  Pkt Lost Ratio: " << ((double)flow->second.txPackets-(double)flow->second.rxPackets)/(double)flow->second.txPackets << std::endl;
+         std::cout << "  Throughput: " << ( ((double)flow->second.rxBytes*8) / (flow->second.timeLastRxPacket.GetSeconds ()) ) << "bps" << std::endl;
+         std::cout << "  Mean{Delay}: " << (flow->second.delaySum.GetSeconds()/flow->second.rxPackets) << std::endl;
+         std::cout << "  Mean{Jitter}: " << (flow->second.jitterSum.GetSeconds()/(flow->second.rxPackets)) << std::endl;
+       }
+     }
+}
+
+
 
 // GlobalValue declarations
 static ns3::GlobalValue g_num_enb("num_enb", "Number of eNBs",
