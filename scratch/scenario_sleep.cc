dc87f85c03c11677eb4101f9e0554fad1ec972d6
Bolun Zhang <bolun_zhangzbl@outlook.com>
2025-03-21 00:55:19 +0800

    20/03/2025; json; 9th

--- scratch/scenario_sleep.cc
+++ scratch/scenario_sleep.cc
@@ -348,54 +348,6 @@ void NetworkScenario::enable_trace()
     this->lte_helper->EnableTraces();
 }
 
-//void NetworkScenario::create_lte_network()
-//{
-//    // Create LTE and EPC helpers. Network to be set up as a bunch of LTE base
-//    // stations (eNodeB), attached to an EPC (network core) implementation and
-//    // UEs (mobile handsets)
-//    this->epc_helper = CreateObject<PointToPointEpcHelper>();
-//    this->lte_helper = CreateObject<LteHelper>();
-//    this->lte_helper->SetEpcHelper(this->epc_helper);
-//
-//    this->lte_helper->SetAttribute ("NumberOfComponentCarriers", UintegerValue (1));
-//    this->lte_helper->SetAttribute ("EnbComponentCarrierManager", StringValue ("ns3::RrComponentCarrierManager"));
-//    this->lte_helper->SetEnbDeviceAttribute ("DlBandwidth", UintegerValue (100));
-//    this->lte_helper->SetEnbDeviceAttribute ("UlBandwidth", UintegerValue (100));
-//
-//    // Set up a directional antenna, to allow 3-sector base stations
-//    // this->lte_helper->SetEnbAntennaModelType("ns3::ParabolicAntennaModel");
-//    // this->lte_helper->SetEnbAntennaModelAttribute("Beamwidth", DoubleValue(70.0));
-//
-//    // Activate handovers using a default RSRQ-based algorithm
-//    this->lte_helper->SetHandoverAlgorithmType("ns3::A2A4RsrqHandoverAlgorithm");
-//    this->lte_helper->SetHandoverAlgorithmAttribute("ServingCellThreshold", UintegerValue(30));
-//
-//
-//    // Select "hard" frequency reuse (FR), which fully partitions the spectrum
-//    // into three equal parts and distributes those among the base stations
-//    this->lte_helper->SetFfrAlgorithmType("ns3::LteFrHardAlgorithm");
-//    // this->lte_helper->SetEnbComponentCarrierManagerType("ns3::RrComponentCarrierManager");
-//
-//
-//    // Config::SetDefault ("ns3::LteHelper::UseCa", BooleanValue (true));
-//   // Config::SetDefault ("ns3::LteHelper::EnbComponentCarrierManager", StringValue ("ns3::RrComponentCarrierManager"));
-//
-//
-//    // Bump the maximum possible number of UEs connected per eNodeB
-//    Config::SetDefault("ns3::LteEnbRrc::SrsPeriodicity", UintegerValue(80));
-//
-//    // Loop through the eNodeB nodes and set up the base stations
-//     for (uint32_t i = 0; i < this->enb_nodes.GetN(); i++) {
-//         Ptr<Node> node = this->enb_nodes.Get(i);
-//         this->lte_helper->SetFfrAlgorithmAttribute(
-//             "FrCellTypeId", UintegerValue((i % 3) + 1));
-//         this->lte_helper->InstallEnbDevice(node);
-//     }
-//
-//    // Add an X2 interface between the eNodeBs, to enable handovers
-//    this->lte_helper->AddX2Interface(this->enb_nodes);
-//
-//}
 
 void NetworkScenario::create_lte_network()
 {
@@ -498,68 +450,6 @@ void NetworkScenario::create_remote_server()
     int server_iface_toward_pgw = server_ifaces.Get(0).second;
     server_routing->AddNetworkRouteTo("7.0.0.0", "255.0.0.0", server_iface_toward_pgw);
 }
-
-//void NetworkScenario::create_ue_applications()
-//{
-//    std::cout << "Installing Internet stack on UEs..." << std::endl;
-//    InternetStackHelper ip_stack_helper;
-//    ip_stack_helper.Install(this->ue_nodes);
-//
-//    std::cout << "Installing LTE devices on UEs..." << std::endl;
-//    NetDeviceContainer ue_devices = this->lte_helper->InstallUeDevice(this->ue_nodes);
-//
-//    std::cout << "Assigning IP addresses to UEs..." << std::endl;
-//    Ipv4InterfaceContainer ue_ifaces = this->epc_helper->AssignUeIpv4Address(ue_devices);
-//
-//    // Verify IPs assigned
-//    for (uint32_t i = 0; i < ue_nodes.GetN(); i++) {
-//        std::cout << "UE " << i << " IP Address: " << ue_ifaces.GetAddress(i) << std::endl;
-//    }
-//
-//    // Attach each UE to the closest eNB
-//    std::cout << "Attaching UEs to eNBs..." << std::endl;
-//    for (uint32_t i = 0; i < this->ue_nodes.GetN(); i++) {
-//        Ptr<LteUeNetDevice> ueDevice = ue_devices.Get(i)->GetObject<LteUeNetDevice>();
-//        Ptr<LteEnbNetDevice> enbDevice = this->enb_nodes.Get(i % this->enb_nodes.GetN())->GetDevice(0)->GetObject<LteEnbNetDevice>();
-//        this->lte_helper->Attach(ueDevice, enbDevice);
-//
-//        // Debug attachment
-//        if (ueDevice->GetRrc()->GetState() == LteUeRrc::CONNECTED_NORMALLY) {
-//            std::cout << "UE " << i << " attached successfully to eNB " << enbDevice->GetCellId() << std::endl;
-//        } else {
-//            std::cout << "ERROR: UE " << i << " NOT attached!" << std::endl;
-//        }
-//    }
-//
-//    // Set default IP route for all UEs
-//    std::cout << "Setting default routes for UEs..." << std::endl;
-//    Ipv4StaticRoutingHelper routing_helper;
-//    for (uint32_t i = 0; i < this->ue_nodes.GetN(); i++) {
-//        Ptr<Ipv4StaticRouting> ueRouting = routing_helper.GetStaticRouting(this->ue_nodes.Get(i)->GetObject<Ipv4>());
-//        ueRouting->SetDefaultRoute(this->epc_helper->GetUeDefaultGatewayAddress(), 1);
-//    }
-//
-//    // Set up traffic generator from server to each UE
-//    std::cout << "Setting up traffic generator..." << std::endl;
-//    const char *socket_factory_type = "ns3::UdpSocketFactory";  // Use UDP for simplicity
-//    for (uint32_t i = 0; i < this->ue_nodes.GetN(); i++) {
-//        InetSocketAddress cbr_dest(ue_ifaces.GetAddress(i), 10000);
-//        OnOffHelper cbr_helper(socket_factory_type, cbr_dest);
-//        cbr_helper.SetConstantRate(DataRate("20Mbps"));
-//        ApplicationContainer cbr_apps = cbr_helper.Install(this->server_nodes.Get(0));
-//        cbr_apps.Start(Seconds(1));
-//        cbr_apps.Stop(Seconds(10)); // Ensure stop time is set
-//
-//        // Set up a sink on the receiving UE
-//        PacketSinkHelper packet_sink_helper(socket_factory_type, cbr_dest);
-//        ApplicationContainer sink_apps = packet_sink_helper.Install(this->ue_nodes.Get(i));
-//        sink_apps.Start(Seconds(0));
-//
-//        std::cout << "Traffic generator set up for UE " << i << " at " << ue_ifaces.GetAddress(i) << std::endl;
-//    }
-//
-//    std::cout << "UE applications created successfully!" << std::endl;
-//}
 
 
 void NetworkScenario::create_ue_applications()
