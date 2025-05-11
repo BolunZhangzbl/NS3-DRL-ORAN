128df68100ea5ceb5510276882db35012223a65b
Bolun Zhang <Bolun_zhangzbl@outlook.com>
2025-05-11 16:29:16 +0100

    11/05/2025; json4base_torch; 3rd

--- nsoran/agents/dqn.py
+++ nsoran/agents/dqn.py
@@ -61,18 +61,15 @@ class BaseAgentDQN:
             def __init__(self, input_dim, output_dim):
                 super(DQNModel, self).__init__()
                 
-                self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3)  # Kernel size = 3
-                # Calculate the output length after convolution
-                conv_output_dim = input_dim - 2  # Since kernel_size = 3, padding is assumed to be 0
-                
+                self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3) 
                 self.flatten = nn.Flatten()
-                self.fc1 = nn.Linear(32 * conv_output_dim, 512)  # Adjust the input dimension based on the conv output
+                self.fc1 = nn.Linear(512, 512)  # Adjust the input dimension based on the conv output
                 self.fc2 = nn.Linear(512, 256)
                 self.output = nn.Linear(256, output_dim)
 
             def forward(self, x):
-                x = torch.relu(self.conv1(x))
-                x = self.flatten(x)
+                # x = torch.relu(self.conv1(x))
+                # x = self.flatten(x)
                 x = torch.relu(self.fc1(x))
                 x = torch.relu(self.fc2(x))
                 x = self.output(x)
